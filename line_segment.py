6d64ae4b17ee9178d530f799b0efbd218d1e6eed
Kristin Evans <kristinlashun@gmail.com>
2023-12-01 19:35:55 -0800

    adjustment for cases

--- line_segment.py
+++ line_segment.py
@@ -3,8 +3,6 @@
 # Date: December 1st, 2023
 # Description: This program defines two classes 'Point' and 'LineSegment' to represent geometric points and line segments. It includes methods to calculate distance, length, slope, and check parallelism.
 
-import math
-
 class Point:
     """Class to represent a point in a 2D space."""
     def __init__(self, x_coord, y_coord):
@@ -22,7 +20,7 @@ class Point:
 
     def distance_to(self, other):
         """Calculate the Euclidean distance to another point."""
-        return math.sqrt((self._x_coord - other.get_x_coord())**2 + (self._y_coord - other.get_y_coord())**2)
+        return ((self._x_coord - other.get_x_coord())**2 + (self._y_coord - other.get_y_coord())**2)**0.5
 
 class LineSegment:
     """Class to represent a line segment in a 2D space defined by two points."""
@@ -45,12 +43,25 @@ class LineSegment:
 
     def slope(self):
         """Calculate the slope of the line segment."""
-        try:
-            return (self._endpoint_2.get_y_coord() - self._endpoint_1.get_y_coord()) / (self._endpoint_2.get_x_coord() - self._endpoint_1.get_x_coord())
-        except ZeroDivisionError:
-            return math.inf  # Slope is infinite for vertical line segments
+        dx = self._endpoint_2.get_x_coord() - self._endpoint_1.get_x_coord()
+        dy = self._endpoint_2.get_y_coord() - self._endpoint_1.get_y_coord()
+        if dx == 0 or dy == 0:  # Handles both vertical lines and points with the same position
+            return None
+        else:
+            return dy / dx
 
     def is_parallel_to(self, other):
         """Check if the line segment is parallel to another line segment."""
-        return math.isclose(self.slope(), other.slope(), abs_tol=1e-9)
+        # If either line segment has length 0, they are not parallel
+        if self.length() == 0 or other.length() == 0:
+            return False
+        # Both segments have a slope of None, which means they are both vertical and hence parallel
+        if self.slope() is None and other.slope() is None:
+            return True
+        # If one slope is None (vertical line) and the other is not, they are not parallel
+        if (self.slope() is None) != (other.slope() is None):
+            return False
+        return self.slope() == other.slope()
+
+
 
